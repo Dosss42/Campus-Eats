414403ba6320044d164c81319db168992a0e7d6d
Dosss42 <programmm29@gmail.com>
2026-09-01 10:24:15 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Campus Eats',
+  webDir: 'www'
+};
+
+export default config;
